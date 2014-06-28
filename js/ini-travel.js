b61945ecc7f39b430e80d4ea511e8c69b69b2c42
mediapole <info@mediapole.org>
2014-06-28 10:03:36 +0200

    Consultation de séjour sur mobile
    Application mobile pour agence de voyage

---
+++ js/ini-travel.js
@@ -0,0 +1,9 @@
+label = new Array();
+
+label['home'] = {fr: "Agence"};
+label['list'] = {fr: "Liste des séjours"};
+label['product'] = {fr: "Séjour"};
+label['favorite'] = {fr: "Favoris"};
+label['contact'] = {fr: "Contact"};
+label['back'] = {fr: "Retour"};
+label['currency'] = {fr: "€"};
